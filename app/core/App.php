d866c928028abc94c07bdb8f64f9b30809f04f9a
arturocampi <arturo.herraez.tpz@gmail.com>
2021-11-09 17:28:13 +0100

    controladora

---
+++ app/core/App.php
@@ -0,0 +1,21 @@
+<?php
+
+class App
+{
+    public function __construct()
+    {
+        session_start();
+        if (isset($_GET['url']) and !empty($_GET['url'])) {
+            $url = $_GET['url'];
+        } else {
+            $url = 'home';
+        }
+
+        $controllerName = array_shift($arguments);
+        if (count($arguments)) {
+            $method =  array_shift($arguments);
+        } else {
+            $method = "index";
+        }
+    }
+}
