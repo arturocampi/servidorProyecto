09d65be3462772cf731c671746bcf42dbdcd33ad
arturocampi <arturo.herraez.tpz@gmail.com>
2021-11-11 20:08:17 +0100

    fixed

--- app/controllers/ClienteController.php
+++ app/controllers/ClienteController.php
@@ -3,11 +3,6 @@
 class ClienteController
 {
 
-    function __construct()
-    {
-        echo "HomeController -> construct <br>";
-    }
-
     public function index()
     {
         require "app/views/cliente.php";
