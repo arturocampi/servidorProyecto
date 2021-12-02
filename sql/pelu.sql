5dea9cf4f93cbeca4d1d78e766c6e0d7d989e91d
arturocampi <arturo.herraez.tpz@gmail.com>
2021-12-02 19:09:04 +0100

    fix

---
+++ sql/pelu.sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: database:3306
+-- Generation Time: Dec 02, 2021 at 05:01 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pelu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_event`
+--
+
+CREATE TABLE `detail_event` (
+  `event_id` int NOT NULL,
+  `line` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `quantity` varchar(50) DEFAULT NULL,
+  `details` varchar(500) DEFAULT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `detail_event`
+--
+
+INSERT INTO `detail_event` (`event_id`, `line`, `name`, `quantity`, `details`, `price`) VALUES
+(1, 1, 'Caracterización completa', '10', '', 100),
+(1, 2, 'Caracterización básica', '10', '', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `surname` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `details` varchar(300) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '0',
+  `admin` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `name`, `surname`, `email`, `details`, `birthdate`, `password`, `active`, `admin`) VALUES
+(1, 'Ana', 'Navarro', 'ana@pelu.com', 'Ana es la responsable del centro desde sus inicios. Es creativa y emprendedora, le encanta aportar nuevas ideas.', '2001-04-01', '$2y$10$b4qbj8k0CNQ0DW4nFfcVQeEMGHfHvryyKdSEThdTavgMamYKWv4fa', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_service`
+--
+
+CREATE TABLE `employee_service` (
+  `employee_id` int NOT NULL,
+  `service_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `employee_service`
+--
+
+INSERT INTO `employee_service` (`employee_id`, `service_id`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `id` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `client` varchar(50) DEFAULT NULL,
+  `details` varchar(500) DEFAULT NULL,
+  `start_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`id`, `name`, `client`, `details`, `start_date`) VALUES
+(1, 'Maria Antonieta', 'Teatro las esquinas', '10 caracterizaciones para personajes prinicpales y 50 secundarios', '2021-09-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `id` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `details` varchar(500) DEFAULT NULL,
+  `price` double NOT NULL,
+  `time` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`id`, `name`, `gender`, `details`, `price`, `time`) VALUES
+(1, 'cortar', 'mujer', 'Cada corte es único y personal', 20, 30),
+(2, 'cortar', 'hombre', 'Cada corte es único y personal', 16, 20),
+(3, 'balayage', 'mujer', 'Nuestra técnica aporta lumnosidad y naturalidad', 45, 60);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_event`
+--
+ALTER TABLE `detail_event`
+  ADD PRIMARY KEY (`event_id`,`line`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `employee_service`
+--
+ALTER TABLE `employee_service`
+  ADD PRIMARY KEY (`employee_id`,`service_id`),
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_event`
+--
+ALTER TABLE `detail_event`
+  ADD CONSTRAINT `detail_event_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`);
+
+--
+-- Constraints for table `employee_service`
+--
+ALTER TABLE `employee_service`
+  ADD CONSTRAINT `employee_service_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
+  ADD CONSTRAINT `employee_service_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
